6715c929aeab2f6aa39f7519d2b9bb1261da6860
Maitén Guadalupe Blanc <116910158+MaitenBlanc@users.noreply.github.com>
2024-04-11 11:58:01 -0300

    Delete inventario directory

--- inventario/src/main/java/gm/inventarios/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package gm.inventarios.repository;
-
-import gm.inventarios.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
